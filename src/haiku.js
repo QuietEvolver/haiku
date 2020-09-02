59ceeeee98d5819a72410ab2f834ecda8ccf55a4
Vera <quietevolver@users.noreply.github.com>
2020-09-02 12:05:12 -0700

    Update haiku.js syllableCount fxn.
    Co-Authored-By: Jeff Kim <kim2jy@hotmail.com>

--- src/haiku.js
+++ src/haiku.js
@@ -1,11 +1,20 @@
 const wordIntake = ``; //pls show me how to do it the way you saw it w/the const=fxn=fatarrow
+
+/*
+ - vowel count: account for every word's vowel input 
+ - if a word ends in (silent) 'e': if last letter is an 'e', subtract 1 from syllables
+- diphthongs: account for iterations of double vowels found
+*/
 const syllableCount = (wordIntake) => {
-  for (let i = wordIntake.length; i <= 0; i--) {
-    if (`a` || `e` || `i` || `o` || `u`) {
-      return i++;
-    } else
-      () => {
-        return i;
-      };
+  let syllables = 0;
+  for (let i = wordIntake.length; i > 0; i--) {
+    if (charAt(i) == 'a' || charAt(i) == 'e' || charAt(i) == 'i' || charAt(i) == 'o' || charAt(i) == 'u') {
+      syllables++;
+    }
+
+    if (charAt(i) == ('a' || 'e') && charAt(i+1) == 'a' || 'e' || 'i' || 'o' || 'u')
+  }
+  if (charAt(wordIntake.length -1) == 'e') {
+    syllables--;
   }
 };
