16e28970e38ff9ef093a90b82d33692da8d80c1c
Vera <quietevolver@users.noreply.github.com>
2020-09-02 13:48:58 -0700

    Update index.html and haiku.js syllableCount fxn.
    Co-Authored-By: Jeff Kim <kim2jy@hotmail.com>

--- src/haiku.js
+++ src/haiku.js
@@ -8,13 +8,26 @@ const wordIntake = ``; //pls show me how to do it the way you saw it w/the const
 const syllableCount = (wordIntake) => {
   let syllables = 0;
   for (let i = wordIntake.length; i > 0; i--) {
-    if (charAt(i) == 'a' || charAt(i) == 'e' || charAt(i) == 'i' || charAt(i) == 'o' || charAt(i) == 'u') {
-      syllables++;
+    if (
+      charAt(i) == "a" ||
+      charAt(i) == "e" ||
+      charAt(i) == "i" ||
+      charAt(i) == "o" ||
+      charAt(i) == "u"
+    ) {
+      if (
+        charAt(i + 1) != "a" &&
+        charAt(i + 1) != "e" &&
+        charAt(i + 1) != "i" &&
+        charAt(i + 1) != "o" &&
+        charAt(i + 1) != "u"
+      ) {
+        syllables++;
+      }
     }
-
-    if (charAt(i) == ('a' || 'e') && charAt(i+1) == 'a' || 'e' || 'i' || 'o' || 'u')
   }
-  if (charAt(wordIntake.length -1) == 'e') {
+  if (charAt(wordIntake.length - 1) == "e") {
     syllables--;
   }
+  return syllables + " syllables";
 };
