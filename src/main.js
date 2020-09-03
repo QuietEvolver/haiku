840685b62482dff55fd13123469a0b9e1324d541
Vera <quietevolver@users.noreply.github.com>
2020-09-03 11:23:43 -0700

    Update main.js FormData Blob value.
    Co-Authored-By: Jeff Kim <kim2jy@hotmail.com>

--- src/main.js
+++ src/main.js
@@ -1,32 +1,44 @@
 /* eslint-disable no-unused-vars */
 import "./styles.css";
 import { syllableCount } from "./haiku";
+//TypeErr: null
 
-const haikuSubmitted = new FormData();
+const haikuFormElem = document.querySelector("#haiku-form");
+haikuFormElem.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-const firstLine = document.getElementById("first-line");
-const secondLine = document.getElementById("second-line");
-const thirdLine = document.getElementById("third-line");
-let //haiku = haikuSubmitted;
-  haiku = document.getElementById.haikuSubmitted(
-    Object.assign({}, ...firstLine, ...secondLine, ...thirdLine)
+  const haikuSubmitted = new FormData(haikuFormElem);
+
+  const firstLine = document.getElementById("first-line");
+  const secondLine = document.getElementById("second-line");
+  const thirdLine = document.getElementById("third-line");
+  let haiku = Object.assign({}, firstLine, secondLine, thirdLine);
+
+  const haikuBlob = new Blob(
+    haikuSubmitted,
+    Blob.prototype.text("#return-haiku-poem")
   );
-//haikuSubmitted.append(firstLine, secondLine, thirdLine);
-function haikuChecker() {
-  document.getElementById("return-first-line-value").innerHTML = syllableCount(
-    haiku.firstLine
-  );
+  //haiku = haikuSubmitted;
 
-  document.getElementById("return-second-line-value").innerHTML = syllableCount(
-    haiku.secondLine
-  );
+  haikuSubmitted.append(firstLine, secondLine, thirdLine, haikuBlob);
 
-  document.getElementById("return-third-line-value").innerHTML = syllableCount(
-    haiku.thirdLine
-  );
-}
+  //document.getElementById.haikuSubmitted(haiku); //??
 
-document.getElementById("haiku-display").innerHTML = haikuChecker();
-//DISPLAY?? let firstLineSyllableCount =  h (syllableCount("First line syllable count is: " + haiku.firstLine + haiku.secondLine + haiku.thirdLine));
+  function haikuChecker() {
+    document.getElementById(
+      "return-first-line-value"
+    ).element.innerHTML = syllableCount(haiku.firstLine);
+
+    document.getElementById(
+      "return-second-line-value"
+    ).element.innerHTML = syllableCount(haiku.secondLine);
+
+    document.getElementById(
+      "return-third-line-value"
+    ).element.innerHTML = syllableCount(haiku.thirdLine);
+  }
+
+  document.getElementById("haiku-display").element.innerHTML = haikuChecker();
+}); //DISPLAY?? let firstLineSyllableCount =  h (syllableCount("First line syllable count is: " + haiku.firstLine + haiku.secondLine + haiku.thirdLine));
 
 //MULT:let new FormData(Object.assign({haiku},firstLine,secondLine,thirdLine)); //IDK if this will work..
